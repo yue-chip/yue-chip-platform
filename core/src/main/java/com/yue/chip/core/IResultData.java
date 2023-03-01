900eadbeaeaf83e92754934a5dc498175d005006
Mr.Liu <158442534@qq.com>
2023-03-01 17:48:35 +0800

    慢慢完善-^_^

--- core/src/main/java/com/yue/chip/core/IResultData.java
+++ core/src/main/java/com/yue/chip/core/IResultData.java
@@ -12,7 +12,7 @@ public interface IResultData<T> {
     @Schema(description = "提示信息")
     public String getMessage();
 
-    @Schema(description = "(-100, \"uri无效\"),(403, \"登录失败\"),(404, \"找不到服务\"),(-200, \"错误\"),(-300, \"请求拒绝\"),(200, \"成功\"),(401, \"无权限\")")
+    @Schema(description = "(-100:uri无效,403:登录失败,404:找不到服务,-200:错误,-300:请求拒绝,200:成功,401:无权限")
     public Integer getStatus();
 
     @Schema(description = "异常信息")
