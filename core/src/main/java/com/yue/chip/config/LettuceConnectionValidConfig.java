32dd85014a9c55f7b881fbc347b13ba19554bae4
Mr.Liu <158442534@qq.com>
2023-06-19 17:09:06 +0800

    ^慢慢完善^

---
+++ core/src/main/java/com/yue/chip/config/LettuceConnectionValidConfig.java
@@ -0,0 +1,25 @@
+package com.yue.chip.config;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Mr.Liu
+ * @date 2023/6/19 上午9:52
+ */
+@Component
+public class LettuceConnectionValidConfig implements InitializingBean {
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if(redisConnectionFactory instanceof LettuceConnectionFactory){
+            LettuceConnectionFactory c=(LettuceConnectionFactory)redisConnectionFactory;
+            c.setValidateConnection(true);
+        }
+    }
+}
