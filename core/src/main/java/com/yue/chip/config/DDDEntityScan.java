2ce7a69aff01e3fdbe0e34a6480ad332447eec39
Mr.Liu <158442534@qq.com>
2023-06-20 13:43:01 +0800

    ^慢慢完善^

--- core/src/main/java/com/yue/chip/config/DDDEntityScan.java
+++ core/src/main/java/com/yue/chip/config/DDDEntityScan.java
@@ -12,7 +12,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplicat
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
 import java.util.Objects;
 import java.util.Set;
 
