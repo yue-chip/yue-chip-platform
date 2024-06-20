b7e5fca83ef2d65924bb271e1f670f87b8dff11e
Mr.Liu <158442534@qq.com>
2024-06-20 16:15:30 +0800

    慢慢完善
    (cherry picked from commit c3d656717fff00207ad185b25b4b44e4862e68ab)

--- core/src/main/java/com/yue/chip/utils/ConnectionSwitchoverUtil.java
+++ core/src/main/java/com/yue/chip/utils/ConnectionSwitchoverUtil.java
@@ -1,11 +1,10 @@
 package com.yue.chip.utils;
 
-import com.yue.chip.exception.BusinessException;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.util.StringUtils;
 
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.SQLException;
 import java.util.Objects;
 
 /**
@@ -19,26 +18,26 @@ public class ConnectionSwitchoverUtil {
         if (Objects.isNull(conn)){
             return;
         }
-        Statement statement = null;
-        String tenantDataBaseName = TenantDatabaseUtil.tenantDatabaseName(TenantNumberUtil.getTenantNumber());
-        if (StringUtils.hasText(tenantDataBaseName)) {
-            try {
-                statement = conn.createStatement();
-                statement.execute(TenantDatabaseUtil.getDatabaseScript(tenantDataBaseName));
-            }catch (Exception exception){
-                BusinessException.throwException("切换数据库失败");
-            }finally {
-                HibernateSessionJdbcUtil.close(statement);
-            }
-        }
-//        try {
-//            String databaseName = TenantDatabaseUtil.tenantDatabaseName(TenantNumberUtil.getTenantNumber());
-//            if (StringUtils.hasText(databaseName)) {
-//                conn.setCatalog(databaseName);
+//        Statement statement = null;
+//        String tenantDataBaseName = TenantDatabaseUtil.tenantDatabaseName(TenantNumberUtil.getTenantNumber());
+//        if (StringUtils.hasText(tenantDataBaseName)) {
+//            try {
+//                statement = conn.createStatement();
+//                statement.execute(TenantDatabaseUtil.getDatabaseScript(tenantDataBaseName));
+//            }catch (Exception exception){
+//                BusinessException.throwException("切换数据库失败");
+//            }finally {
+//                HibernateSessionJdbcUtil.close(statement);
 //            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            throw new RuntimeException(e);
 //        }
+        try {
+            String databaseName = TenantDatabaseUtil.tenantDatabaseName(TenantNumberUtil.getTenantNumber());
+            if (StringUtils.hasText(databaseName)) {
+                conn.setCatalog(databaseName);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 }
